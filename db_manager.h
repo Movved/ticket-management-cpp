44a250234472d9455e2fd0fa07ef9abfab6b456f
Movved <mohabellatrach@gmail.com>
2026-01-14 18:57:41 +0100

    db_manager methods (.h)

---
+++ db_manager.h
@@ -0,0 +1,21 @@
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
+
+class DBManager {
+private:
+    sql::Driver* driver;
+    sql::Connection* conn;
+
+public:
+    DBManager(const char* host, const char* user, const char* password, const char* database);
+    ~DBManager();
+
+    void addTicket(const char* title, const char* description, const char* status, const char* created_by);
+
+     void listTickets() const;
+  //  void updateTicketStatus(const int& id, const char* status);
+    //void deleteTicket(const int& id);
+};
