678d43b76e35b5b755db9c87a576ec6455a222ca
Movved <mohabellatrach@gmail.com>
2026-01-14 18:56:59 +0100

    ticket.h and cpp

--- ticket.cpp
+++ ticket.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
+#include <cstring>
 #include "ticket.h"
 using namespace std;
+
+ticket::ticket(const char* titre_,const char* description_,const char* statut_,const char* creePar_)
+{
+    titre = new char[(strlen(titre_)+1)];
+    description = new char[(strlen(description_)+1)];
+    statut = new char[(strlen(statut_)+1)];
+    creePar = new char[(strlen(creePar_)+1)];
+
+    strcpy(titre,titre_);
+    strcpy(description,description_);
+    strcpy(statut,statut_);
+    strcpy(creePar,creePar_);
+}
+ticket::~ticket()
+{
+    delete [] titre;
+    delete [] description;
+    delete [] statut;
+    delete [] creePar;
+}
+void ticket::setStatut(const char* nvStatut)
+{
+    statut = new char[(strlen(nvStatut)+1)];
+    strcpy(statut,nvStatut);
+}
--- ticket.h
+++ ticket.h
@@ -1,8 +1,15 @@
 class ticket{
 public :
-
-
+    ticket(const char* titre_,const char* description_,const char* statut_,const char* creePar_);
+    ~ticket();
+    char* getTitre() const { return titre; }
+    char* getDescription() const { return description; }
+    char* getStatut() const { return statut; }
+    char* getCreePar() const { return creePar; }
+    void setStatut(const char* nvStatut);
 private:
-
-
+    char* titre;
+    char* description;
+    char* statut;
+    char* creePar;
 };
